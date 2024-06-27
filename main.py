15dc7e1aa647ee3458b86a6a9eeef3073979470f
Ricardo Brito do Nascimento <britodfbr@gmail.com>
2024-06-27 10:26:18 -0300

    refactor: Alterado GUI de PySimpleGUI para Flet

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 """Main module."""
 
 # ruff: noqa: ERA001
-# from incolume.py.sorteio_xlsx.gui import ft
-from incolume.py.sorteio_xlsx.gui import psg
+import flet
+from incolume.py.sorteio_xlsx.gui import ft
+# from incolume.py.sorteio_xlsx.gui import psg
 
 if __name__ == '__main__':
-    # flet.app(ft.interface_gui)
-    psg.interface_gui()
+    flet.app(ft.interface_gui)
+    # psg.interface_gui()
