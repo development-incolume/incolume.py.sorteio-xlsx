0d97939bbc02f27af66c7c729bb56b78cc8352aa
Ricardo Brito do Nascimento <britodfbr@gmail.com>
2024-06-28 09:58:08 -0300

    feat: tratativas em captura OS para arquivos excel

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 
 # ruff: noqa: ERA001
 import flet
+
 # from incolume.py.sorteio_xlsx.gui import ft
-from incolume.py.sorteio_xlsx.gui import tratativa1 as t1
+from incolume.py.sorteio_xlsx.gui import tratativas as t1
 # from incolume.py.sorteio_xlsx.gui import psg
 
 if __name__ == '__main__':
-    flet.app(t1.main)
+    flet.app(t1.tratativa3)
     # flet.app(ft.interface_gui)
     # psg.interface_gui()
