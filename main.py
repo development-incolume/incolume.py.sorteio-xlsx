f472ca1a843a297fd62498e622504b5a16b128ce
Ricardo Brito do Nascimento <britodfbr@gmail.com>
2024-06-28 09:16:10 -0300

    feat: exemplo oficial para FilePickle

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 
 # ruff: noqa: ERA001
 import flet
-from incolume.py.sorteio_xlsx.gui import ft
+# from incolume.py.sorteio_xlsx.gui import ft
+from incolume.py.sorteio_xlsx.gui import tratativa1 as t1
 # from incolume.py.sorteio_xlsx.gui import psg
 
 if __name__ == '__main__':
-    flet.app(ft.interface_gui)
+    flet.app(t1.main)
+    # flet.app(ft.interface_gui)
     # psg.interface_gui()
